4864c50857e079332c3cdf24c8d075382b4c9b31
jojoAxolotl <jasephine724@gmail.com>
2024-02-29 17:22:40 +0800

    334

---
+++ Medium/334.py
@@ -0,0 +1,17 @@
+# 334. Increasing Triplet Subsequence
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        smallest = sys.maxint
+        smaller = sys.maxint
+        for num in nums:
+            if num <= smallest:
+                smallest = num
+            elif num <= smaller:
+                smaller = num
+            else:
+                return True
+        return False
