c2dfac51c8bd791e05f4aba5de2a0d78a54fd3fa
jojoAxolotl <jasephine724@gmail.com>
2024-03-30 13:56:01 +0800

    206

---
+++ Easy/206.py
@@ -0,0 +1,18 @@
+# 206. Reverse Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        result = None 
+        while cur:
+            next_node = cur.next
+            cur.next = result
+            result = cur
+            cur = next_node      
+        return result
