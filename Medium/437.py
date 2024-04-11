4a58de2805d670ac489bfaf35909fcdc7a9a6db7
jojoAxolotl <jasephine724@gmail.com>
2024-04-11 23:14:13 +0800

    437 re-read

--- Medium/437.py
+++ Medium/437.py
@@ -13,17 +13,21 @@ from typing import TreeNode
 
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        # 用來記錄從根節點到當前節點的所有可能的總和值，以及每個總和值出現的次數。
         d = collections.Counter()
         d[0] = 1
         
+        # depth-first search 
         def dfs(node: Optional[TreeNode], s: int) -> int:
-            if not node: return 0
+            if not node: 
+                return 0
             s += node.val
             res =  d[s - targetSum]
             d[s] += 1
             res += dfs(node.left, s) + dfs(node.right, s)
             d[s] -= 1
-            if d[s] == 0: del d[s]
+            if d[s] == 0: 
+                del d[s]
             return res
         
         return dfs(root, 0)
