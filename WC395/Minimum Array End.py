579edc6dc3f0b9e790aa58c5019df911651f9845
jojoAxolotl <jasephine724@gmail.com>
2024-04-29 11:46:43 +0800

    3133 try

--- WC395/Minimum Array End.py
+++ WC395/Minimum Array End.py
@@ -1,4 +1,7 @@
 # 100282. Minimum Array End
+# 3133.   Minimum Array End
+
+'''
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
         nums = [x]
@@ -16,3 +19,23 @@ class Solution:
     
     # Time Limit Exceeded 
     # Last executed input: 6715154 7193485
+'''
+
+'''
+# time complexity: O(n)
+# a little bit improvement
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        length = 1
+        current = x
+
+        while length < n:
+            next_num = current + 1
+            # 保持 next_num 的所有 x 中的位不變，並逐步增加最小可能值。
+            while (next_num & x) != x:
+                next_num += 1
+            current = next_num
+            length += 1
+
+        return current
+'''
